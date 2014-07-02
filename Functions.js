58988290a54651559d5067c1499a9a81581b7507
hassanjalilk <hjalil.hba2014@ivey.ca>
2014-07-02 15:46:01 -0400

    fortune teller in function

---
+++ Functions.js
@@ -0,0 +1,10 @@
+function tellFortune() {
+	var noc = "No Idea";
+	var pn = "No Clue";
+	var gl = "Pak";
+	var jt = "CEO maybe?!";
+  return "I will be a " + jt + " in " + gl + ", and married to " + pn + " with " + noc + " kids!";
+}
+
+document.write(tellFortune());
+console.log(tellFortune());
