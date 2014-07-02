13708ef0f99cc37b71bb14220379e73dd46056b7
hassanjalilk <hjalil.hba2014@ivey.ca>
2014-07-02 15:55:52 -0400

    age calculator function

--- Functions.js
+++ Functions.js
@@ -1,3 +1,6 @@
+
+// fortune teller
+
 function tellFortune() {
 	var noc = "No Idea";
 	var pn = "No Clue";
@@ -8,3 +11,17 @@ function tellFortune() {
 
 document.write(tellFortune());
 console.log(tellFortune());
+
+
+
+// age calculator
+
+function ageCalculator() {
+	var cy = 2014;
+	var by = 1980;
+	var byy = 1985;
+	return "they are either " + (cy - by) + " or " + (cy - byy) + "";
+}
+
+document.write(ageCalculator());
+console.log(ageCalculator());
