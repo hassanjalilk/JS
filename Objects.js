2ef92c1b0a5ca38814b3851486370a5a733fcf3c
hassanjalilk <hjalil.hba2014@ivey.ca>
2014-07-03 15:26:32 -0400

    objects first two question

---
+++ Objects.js
@@ -0,0 +1,73 @@
+
+var favRecipe = {
+	title: "omg",
+	servings: "5",
+	ingredients: ["a", "b", "c"]
+};
+
+
+	console.log(favRecipe.title);
+	console.log(favRecipe.servings);
+	console.log(favRecipe.ingredients);
+
+
+// --------------------	
+
+var books = [
+	{title: "kite Runner",
+	 authur: "khaled hussaini",
+	 read: "yes"},
+
+	{title: "A", 
+	 authur: "B", 
+	 read: "no"},
+
+	{title: "C", 
+	 authur: "D", 
+	 read: "yes"},
+
+	{title: "E", 
+	 authur: "F", 
+	 read: "no"}
+];
+
+console.log(books[0].title + " " + books[0].authur + " " + books[0].read);
+console.log(books[1].title + " " + books[1].authur + " " + books[1].read);
+console.log(books[2].title + " " + books[2].authur + " " + books[2].read);
+console.log(books[3].title + " " + books[3].authur + " " + books[3].read);
+
+// -------------
+
+
+var movie = [
+	{title: "kite Runner",
+	 duration: "3 hours",
+	 stars: "5"},
+
+	{title: "kite Runner",
+	 duration: "3 hours",
+	 stars: "5"},
+
+	{title: "kite Runner",
+	 duration: "3 hours",
+	 stars: "5"},
+
+	{title: "kite Runner",
+	 duration: "3 hours",
+	 stars: "5"}
+];
+
+
+
+function printMovie(movie) {
+	console.log(movie[0].title);
+}
+
+
+
+
+
+
+
+
+
