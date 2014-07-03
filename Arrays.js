6b918a2d3283612a30bd375058cc8ee95426a114
hassanjalilk <hjalil.hba2014@ivey.ca>
2014-07-02 22:02:58 -0400

    Arrays

---
+++ Arrays.js
@@ -0,0 +1,4 @@
+
+var cars = ["Saab", "Volvo", "BMW"];
+
+console.log("My fav car is " + cars[2]) 
--- Control_Flow.js
+++ Control_Flow.js
@@ -18,3 +18,41 @@ else
 // --------------------
 
 
+var asignGrade = 65
+
+if (assignGrade > 65) {
+console.log("A") }
+else {
+console.log("B")}
+
+// --------
+
+var translator = "en"
+
+if (translator == "en") {
+console.log("Hello, world") }
+else {
+console.log("B")}
+
+
+// ----
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
